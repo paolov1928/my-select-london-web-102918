524784c0867d9ef61e26bb4f235b9f9845f4542d
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-11 15:31:43 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,18 @@
 def my_select(array)
 i = 0
-p=0
+h = 0
 array2 = []
 array3 = []
   while i < array.length
     array2.push(yield array[i])
     i = i + 1
   end
-  array2
+  while h < array.length
+  if array2[h] == true
+  
+  h += 1
+  else
+  h += 1
+  end
+  end
 end
