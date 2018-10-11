656b03cda837403db8b32946c6b46eee406ce84c
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-11 15:22:26 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,12 @@ def my_select(array)
 i = 0
 array2 = []
   while i < array.length
+    if (yield array[i] == true)
     array2.push(yield array[i])
     i = i + 1
+    else
+    i = i + 1
+    end
   end
   array2
 end
