3f5a33e17ca7be40f5387248a459b9de74af0bd1
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-11 15:35:38 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+i = 0
+h = 0
+array2 = []
+array3 = []
+  while i < array.length
+    array2.push(yield array[i])
+    i = i + 1
+  end
+  while h < array.length
+  if array2[h] == true
+  array3.push(array[h])
+  h += 1
+  else
+  h += 1
+  end
+  end
+  array3
 end
